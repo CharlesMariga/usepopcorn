b889eb19292e5662cd8ab36307910cdbc7b72259
CharlesMariga <charlesmariga37@gmail.com>
2023-07-21 06:13:35 +0300

    feat: fixing prop drilling with component composition

--- src/App.js
+++ src/App.js
@@ -55,18 +55,25 @@ export default function App() {
 
   return (
     <>
-      <Navbar movies={movies} />
-      <Main movies={movies} />
+      <Navbar>
+        <Search />
+        <NumResults movies={movies} />
+      </Navbar>
+      <Main>
+        <ListBox>
+          <MovieList movies={movies} />
+        </ListBox>
+        <WatchedBox />
+      </Main>
     </>
   );
 }
 
-function Navbar({ movies }) {
+function Navbar({ children }) {
   return (
     <nav className="nav-bar">
       <Logo />
-      <Search />
-      <NumResults movies={movies} />
+      {children}
     </nav>
   );
 }
@@ -102,27 +109,22 @@ function NumResults({ movies }) {
   );
 }
 
-function Main({ movies }) {
-  return (
-    <main className="main">
-      <ListBox movies={movies} />
-      <WatchedBox />
-    </main>
-  );
+function Main({ children }) {
+  return <main className="main">{children}</main>;
 }
 
-function ListBox({ movies }) {
+function ListBox({ children }) {
   const [isOpen1, setIsOpen1] = useState(true);
 
   return (
     <div className="box">
-      <buttonconst
+      <button
         className="btn-toggle"
         onClick={() => setIsOpen1((open) => !open)}
       >
         {isOpen1 ? "–" : "+"}
-      </buttonconst>
-      {isOpen1 && <MovieList movies={movies} />}
+      </button>
+      {isOpen1 && children}
     </div>
   );
 }
